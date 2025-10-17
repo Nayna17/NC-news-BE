a7de5d1fbb1f4a471a818d80a27e097225003d38
Nayna Mittu <nayna@Naynas-MBP.Home>
2025-10-17 16:59:38 +0100

    inserted the relevant data into topics, users, articles and comments

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,10 @@
 const db = require("../connection");
+const {format} = require("node-pg-format");
+const {createLookupObj, convertTimestampToDate} = require("../seeds/utils")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(`DROP TABLE IF EXISTS comments`)
+  return db
+    .query(`DROP TABLE IF EXISTS comments`)
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS articles`);
     })
@@ -22,7 +25,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       return db.query(`CREATE TABLE users (
       username VARCHAR(50) PRIMARY KEY,
       name VARCHAR(50),
-      avatar_url VARCHAR (1000)
+      avatar_url VARCHAR(1000)
       );`);
     })
     .then(() => {
@@ -34,7 +37,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       body TEXT,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       votes INT DEFAULT 0,
-      article_img_url VARCHAR (1000)
+      article_img_url VARCHAR(1000)
       );`);
     })
     .then(() => {
@@ -45,13 +48,38 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       votes INT DEFAULT 0,
       author VARCHAR(15) REFERENCES users(username),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
-    }).then(() => {
-      const formattedTopics = format(
-        `INSERT INTO topics
-        ()`
-      )
     })
-};
+    .then(() => {
+      const formattedTopics = topicData.map((topic) => {
+        return [topic.slug, topic.description, topic.img_url];
+      });
+      const sqlString = format(`INSERT INTO topics (slug, description, img_url) VALUES %L`, formattedTopics)
+    
+        return db.query(sqlString);
+    }).then(() => {
+      const formattedUsers = userData.map((users) => {
+        return [users.username, users.name, users.avatar_url];
 
+      });
+      const sqlString = format(`INSERT INTO users (username, name, avatar_url) VALUES %L`, formattedUsers)
+      return db.query(sqlString)
+    }).then(() => {
+      const formattedArticles = articleData.map((articles) => {
+        return [articles.title, articles.topic, articles.author, articles.body, convertTimestampToDate(articles).created_at, articles.votes, articles.article_img_url];
+
+      });
+      const sqlString = format(`INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *`, formattedArticles)
+      return db.query(sqlString)
+  
+    }).then(({rows}) => {
+      const lookUp = createLookupObj(rows)   
+      const formattedComments = commentData.map((comments) => {
+        return [lookUp[comments.article_title], comments.body, comments.votes, comments.author, convertTimestampToDate(comments).created_at]
+      })
+      const sqlString = format(`INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L`, formattedComments)
+      return db.query(sqlString)
+    })
+    
+   }
 
 module.exports = seed;
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,9 +1,33 @@
 const db = require("../../db/connection");
 
-exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
+function convertTimestampToDate ({ created_at, ...otherProperties }) {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
 
+function createLookupObj(articles) {
+  const lookupObj = {};
+  
+  articles.forEach((article) => {
+  const title = article.title
+  const id = article.article_id
 
+  lookupObj[title] = id
+  
+  })
+  return lookupObj
+}
+
+module.exports = {createLookupObj, convertTimestampToDate};
+
+    // {
+    //       article_id: 4,
+    //       title: 'Student SUES Mitch!',
+    //       topic: 'mitch',
+    //       author: 'rogersop',
+    //       body: 'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+    //       created_at: 2020-05-06T01:14:00.000Z,
+    //       votes: 0,
+    //       article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    //     },
