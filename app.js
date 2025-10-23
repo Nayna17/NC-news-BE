0e621a68b9961a22a45983074ff63224a9f4cb2c
Nayna Mittu <nayna@Naynas-MBP.Home>
2025-10-23 12:34:21 +0100

    refactor to use controller and model structure

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 const db = require("./db/connection.js");
 const express = require("express");
-// const topics = require("./db/data/test-data/topics.js");
-// const articles = require("./db/data/test-data/articles.js");
-// const users = require("./db/data/test-data/users.js")
 const app = express();
+const { getTopics } = require("./controllers/topics.controllers.js");
+const {
+  getArticles,
+  getArticleById,
+} = require("./controllers/articles.controllers.js");
+const { getUsers } = require("./controllers/users.controllers.js");
 
-app.get("/api/topics", (req, res) => {
-  return db.query("SELECT * FROM topics").then(({ rows }) => {
-    res.status(200).send({ topics: rows });
-  });
-});
+app.get("/api/topics", getTopics);
 
-app.get("/api/articles", (req, res) => {
-  return db
-    .query(
-      `SELECT 
-    article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY articles.created_at DESC`
-    )
-    .then(({ rows }) => {
-      console.log({ articles: rows });
-      res.status(200).send({ articles: rows });
-    });
-});
+app.get("/api/articles", getArticles);
 
-app.get("/api/users", (req, res) => {
-  return db.query(`SELECT * FROM users`).then(({ rows }) => {
-    res.status(200).send({ users: rows });
-  });
-});
+app.get("/api/users", getUsers);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 module.exports = app;
