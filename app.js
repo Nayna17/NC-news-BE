ed9587ecc4d9356f3896cfe74798fd2bd49f0b64
Nayna Mittu <nayna@Naynas-MBP.Home>
2025-11-07 16:19:23 +0000

    add console log

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.patch("/api/articles/:article_id", patchArticleById);
 app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.use((err, req, res, next) => {
-  console.error(error);
+  console.log(err);
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
